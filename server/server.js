3753753c740ea3db94a5b087384672ef299edde1
Ariel <aballesterov@itechcr.com>
2020-03-02 10:30:17 -0600

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -9,13 +9,14 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
 
-app.use(require('./routes/usuario'));
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
 
 
